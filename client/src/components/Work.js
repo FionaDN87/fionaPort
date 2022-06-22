4692eaab154f6b55d10e4b09b0877ad40eb4bf2a
Fiona Nguyen <56050215+FionaDN87@users.noreply.github.com>
2022-06-21 22:56:33 -0500

    Update Work.js

--- client/src/components/Work.js
+++ client/src/components/Work.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import MediaCard from './ProjectCard.js';
+import OnlineStoreImage from '.././images/Online_Store.png';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
 
 const Work = () => {
-    return(
+    return( 
         <div>
-            UNDER DEVELOPMENT
+            
+        <div>
+            {/* <MediaCard 
+            text="Test Text" 
+            url={OnlineStoreImage}
+            projectName="Online Store"/>
+
+            <MediaCard 
+            text="Test Text" 
+            url={OnlineStoreImage}
+            projectName="Online Store"/> */}
+
+<Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+        <div>TEST</div>
+      </Grid>
+    </Box>
+        </div>
         </div>
     )
 }
