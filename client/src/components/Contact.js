4e118cc3905ff73cd2dc5650854a0db9dca89790
Fiona Diep Nguyen <56050215+FionaDN87@users.noreply.github.com>
2022-06-27 21:37:58 -0500

    Implemented Contact Page with EmailJS

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -1,11 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import emailjs from 'emailjs-com';
 
 const Contact = () => {
-    return(
+    const submit = () => {
+        if (name && email && message) {
+           // TODO - send mail
+           const serviceId = 'service_vdb5u4t';
+           const templateId = 'template_o9rossp';
+           const userId = 'OQkpPSe66j-TNNpSX';
+           const templateParams = {
+               name,
+               email,
+               message
+           };
+
+           emailjs.send(serviceId, templateId, templateParams, userId)
+               .then(response => console.log(response))
+               .then(error => console.log(error));
+
+            setName('');
+            setEmail('');
+            setMessage('');
+            setEmailSent(true);
+        } else {
+            alert('Please fill in all fields.');
+        }
+    }
+    const isValidEmail = email => {
+        const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return regex.test(String(email).toLowerCase());
+    };
+
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+    const [emailSent, setEmailSent] = useState(false);
+    return (
         <div>
-            UNDER DEVELOPMENT
+        <input type="text" placeholder="Your Name" value={name} onChange={e => setName(e.target.value)} />
+        <input type="email" placeholder="Your email address" value={email} onChange={e => setEmail(e.target.value)} />
+        <textarea placeholder="Your message" value={message} onChange={e => setMessage(e.target.value)}></textarea>
+        <button onClick={submit}>Send Message</button>
+        <span className={emailSent ? 'visible' : null}>Thank you for your message, Fiona Nguyen will get in touch with you soon.</span>
         </div>
-    )
-}
+    );
+};
 
 export default Contact;
