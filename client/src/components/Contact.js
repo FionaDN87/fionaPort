fb9f2df2d7f943267d2c788e90e136a4a6757b3b
Fiona Nguyen <56050215+FionaDN87@users.noreply.github.com>
2022-06-28 19:32:25 -0500

    Update Contact.js

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -1,10 +1,14 @@
 import React, { useState } from 'react';
 import emailjs from 'emailjs-com';
+import TextField from '@mui/material/TextField';
+import '.././styles/contactPageStyle.css';
+import Box from '@mui/material/Box';
+import { formControlClasses } from '@mui/material';
+import { auto } from '@popperjs/core';
 
 const Contact = () => {
     const submit = () => {
         if (name && email && message) {
-           // TODO - send mail
            const serviceId = 'service_vdb5u4t';
            const templateId = 'template_o9rossp';
            const userId = 'OQkpPSe66j-TNNpSX';
@@ -28,20 +32,50 @@ const Contact = () => {
     }
     const isValidEmail = email => {
         const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        return regex.test(String(email).toLowerCase());
+        if(!regex.test(email))
+        return <div>'Invalid Email Address'</div>
+        // return regex.test(String(email).toLowerCase());
     };
+
+    const EmailValidation = (event) => {
+        var form = document.getElementById("form");
+        var email = document.getElementById("email");
+        const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+     
+        if(email.match(regex)){
+            return;
+        }
+    } 
 
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [message, setMessage] = useState('');
     const [emailSent, setEmailSent] = useState(false);
     return (
-        <div>
-        <input type="text" placeholder="Your Name" value={name} onChange={e => setName(e.target.value)} />
-        <input type="email" placeholder="Your email address" value={email} onChange={e => setEmail(e.target.value)} />
-        <textarea placeholder="Your message" value={message} onChange={e => setMessage(e.target.value)}></textarea>
-        <button onClick={submit}>Send Message</button>
-        <span className={emailSent ? 'visible' : null}>Thank you for your message, Fiona Nguyen will get in touch with you soon.</span>
+        <div className='inputStyle'>
+        <h3>Leave a message</h3> 
+        <input 
+        type="text" 
+        placeholder="Enter your Name" 
+        value={name} 
+        onChange={e => setName(e.target.value)} />
+
+        <input 
+        type="email" 
+        placeholder="Enter your email address" 
+        value={email} 
+        onChange={
+            e => setEmail(e.target.value)} />
+        
+        
+        <textarea style={{width:"100%", height: "200px"}} 
+        placeholder="Enter your message" 
+        value={message} 
+        onChange={e => setMessage(e.target.value)}>
+
+        </textarea>
+        {emailSent && <p>Thank you for your message, I will get in touch with you soon.</p>}
+            <button className='buttonStyle' onClick={submit}>Send Message</button>
         </div>
     );
 };
