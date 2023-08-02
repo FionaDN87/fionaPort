092ad9f2993e7b4115b915759a86f20f6b6dcf5d
Fiona Diep Nguyen <56050215+FionaDN87@users.noreply.github.com>
2023-08-02 01:07:38 -0500

    Updates

---
+++ precache-manifest.647c1f5fb22c451769b069e9dee5ec16.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "0d2acc0b55d634a872f4",
+    "url": "/fionaPort/static/css/main.67dbd75c.chunk.css"
+  },
+  {
+    "revision": "0d2acc0b55d634a872f4",
+    "url": "/fionaPort/static/js/main.0d2acc0b.chunk.js"
+  },
+  {
+    "revision": "31b612338aff5091ed3b",
+    "url": "/fionaPort/static/js/1.31b61233.chunk.js"
+  },
+  {
+    "revision": "ec6b06e27f10e8fd4319",
+    "url": "/fionaPort/static/js/2.ec6b06e2.chunk.js"
+  },
+  {
+    "revision": "785a6e5574196af092e3",
+    "url": "/fionaPort/static/js/runtime~main.785a6e55.js"
+  },
+  {
+    "revision": "673aeaf54b0c941cd89bbc76f11eb433",
+    "url": "/fionaPort/static/media/fiona-jul-2023.673aeaf5.jpg"
+  },
+  {
+    "revision": "0b7577202425f5e50d67b5e0f1852e67",
+    "url": "/fionaPort/static/media/Nguyen_Fiona_Resume_AUG_2023.0b757720.pdf"
+  },
+  {
+    "revision": "124ecc49d3256f49c9c6715be0458c2f",
+    "url": "/fionaPort/static/media/Online_Store.124ecc49.PNG"
+  },
+  {
+    "revision": "09967b3aae950179745c678f06e2198c",
+    "url": "/fionaPort/static/media/HandiparkImage.09967b3a.PNG"
+  },
+  {
+    "revision": "79ce3c1da121d8f13c93676e1e978d7c",
+    "url": "/fionaPort/static/media/SimsoImage.79ce3c1d.PNG"
+  },
+  {
+    "revision": "2612da4395eeb5b3f8878e2f47dfc355",
+    "url": "/fionaPort/static/media/PawprintImage.2612da43.PNG"
+  },
+  {
+    "revision": "57e6f1627bca25971ab41b5a78b71b94",
+    "url": "/fionaPort/index.html"
+  }
+];
